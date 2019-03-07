79b045b5d42ccc3f8dd8736dd9229c1fc18ede5b
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-07 11:11:57 +0300

    Add project files.

---
+++ CoreGiris/Models/MyContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreGiris.Models
+{
+    public class MyContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=UrunYonetimDb;Trusted_Connection=true;");
+        }
+        protected override void OnModelCreating( ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Product>
+        }
+
+
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+    }
+}
