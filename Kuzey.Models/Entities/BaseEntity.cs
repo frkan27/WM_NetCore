2fffc957d62dd0a2dc2929f82dccfbaae4c5c237
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-18 11:47:52 +0300

    Entitiyler

---
+++ Kuzey.Models/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kuzey.Models.Entities
+{
+   public abstract class BaseEntity<T>
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ Kuzey.Models/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kuzey.Models.IdentityEntities
+{
+    class ApplicationUser:IdentityUser
+    {
+    }
+}
